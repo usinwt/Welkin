30bc9d5881e89054085a28d38daaf751e4e27720
usinwt <76551874+usinwt@users.noreply.github.com>
2025-02-03 19:47:23 +0800

    Update welkin.sh

--- welkin.sh
+++ welkin.sh
@@ -36,3 +36,312 @@ echo -e "${gl_zi}curl --version${gl_bai}"
 echo -e "${gl_zi}git --version${gl_bai}"
 
 echo -e "${gl_lv}脚本执行完成！${gl_bai}"
+
+install_add_docker_cn() {
+
+local country=$(curl -s ipinfo.io/country)
+if [ "$country" = "CN" ]; then
+	cat > /etc/docker/daemon.json << EOF
+{
+  "registry-mirrors": [
+	"https://docker-0.unsee.tech",
+	"https://docker.1panel.live",
+	"https://registry.dockermirror.com",
+	"https://docker.imgdb.de",
+	"https://docker.m.daocloud.io",
+	"https://hub.firefly.store",
+	"https://hub.littlediary.cn",
+	"https://hub.rat.dev",
+	"https://dhub.kubesre.xyz",
+	"https://cjie.eu.org",
+	"https://docker.1panelproxy.com",
+	"https://docker.hlmirror.com",
+	"https://hub.fast360.xyz",
+	"https://dockerpull.cn",
+	"https://cr.laoyou.ip-ddns.com",
+	"https://docker.melikeme.cn",
+	"https://docker.kejilion.pro"
+  ]
+}
+EOF
+fi
+
+
+enable docker
+start docker
+restart docker
+
+}
+
+
+install_add_docker_guanfang() {
+local country=$(curl -s ipinfo.io/country)
+if [ "$country" = "CN" ]; then
+	cd ~
+	curl -sS -O ${gh_proxy}raw.githubusercontent.com/kejilion/docker/main/install && chmod +x install
+	sh install --mirror Aliyun
+	rm -f install
+else
+	curl -fsSL https://get.docker.com | sh
+fi
+install_add_docker_cn
+
+
+}
+
+
+
+install_add_docker() {
+	echo -e "${gl_huang}正在安装docker环境...${gl_bai}"
+	if  [ -f /etc/os-release ] && grep -q "Fedora" /etc/os-release; then
+		install_add_docker_guanfang
+	elif command -v dnf &>/dev/null; then
+		dnf update -y
+		dnf install -y yum-utils device-mapper-persistent-data lvm2
+		rm -f /etc/yum.repos.d/docker*.repo > /dev/null
+		country=$(curl -s ipinfo.io/country)
+		arch=$(uname -m)
+		if [ "$country" = "CN" ]; then
+			curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo | tee /etc/yum.repos.d/docker-ce.repo > /dev/null
+		else
+			yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null
+		fi
+		dnf install -y docker-ce docker-ce-cli containerd.io
+		install_add_docker_cn
+
+	elif [ -f /etc/os-release ] && grep -q "Kali" /etc/os-release; then
+		apt update
+		apt upgrade -y
+		apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+		rm -f /usr/share/keyrings/docker-archive-keyring.gpg
+		local country=$(curl -s ipinfo.io/country)
+		local arch=$(uname -m)
+		if [ "$country" = "CN" ]; then
+			if [ "$arch" = "x86_64" ]; then
+				sed -i '/^deb \[arch=amd64 signed-by=\/etc\/apt\/keyrings\/docker-archive-keyring.gpg\] https:\/\/mirrors.aliyun.com\/docker-ce\/linux\/debian bullseye stable/d' /etc/apt/sources.list.d/docker.list > /dev/null
+				mkdir -p /etc/apt/keyrings
+				curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker-archive-keyring.gpg > /dev/null
+				echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker-archive-keyring.gpg] https://mirrors.aliyun.com/docker-ce/linux/debian bullseye stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+			elif [ "$arch" = "aarch64" ]; then
+				sed -i '/^deb \[arch=arm64 signed-by=\/etc\/apt\/keyrings\/docker-archive-keyring.gpg\] https:\/\/mirrors.aliyun.com\/docker-ce\/linux\/debian bullseye stable/d' /etc/apt/sources.list.d/docker.list > /dev/null
+				mkdir -p /etc/apt/keyrings
+				curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker-archive-keyring.gpg > /dev/null
+				echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/docker-archive-keyring.gpg] https://mirrors.aliyun.com/docker-ce/linux/debian bullseye stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+			fi
+		else
+			if [ "$arch" = "x86_64" ]; then
+				sed -i '/^deb \[arch=amd64 signed-by=\/usr\/share\/keyrings\/docker-archive-keyring.gpg\] https:\/\/download.docker.com\/linux\/debian bullseye stable/d' /etc/apt/sources.list.d/docker.list > /dev/null
+				mkdir -p /etc/apt/keyrings
+				curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker-archive-keyring.gpg > /dev/null
+				echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian bullseye stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+			elif [ "$arch" = "aarch64" ]; then
+				sed -i '/^deb \[arch=arm64 signed-by=\/usr\/share\/keyrings\/docker-archive-keyring.gpg\] https:\/\/download.docker.com\/linux\/debian bullseye stable/d' /etc/apt/sources.list.d/docker.list > /dev/null
+				mkdir -p /etc/apt/keyrings
+				curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker-archive-keyring.gpg > /dev/null
+				echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian bullseye stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+			fi
+		fi
+		apt update
+		apt install -y docker-ce docker-ce-cli containerd.io
+		install_add_docker_cn
+
+
+	elif command -v apt &>/dev/null || command -v yum &>/dev/null; then
+		install_add_docker_guanfang
+	else
+		install docker docker-compose
+		install_add_docker_cn
+
+	fi
+	sleep 2
+}
+
+
+install_docker() {
+	if ! command -v docker &>/dev/null; then
+		install_add_docker
+	else
+		echo -e "${gl_lv}Docker环境已经安装${gl_bai}"
+	fi
+}
+
+
+docker_ps() {
+while true; do
+	clear
+	send_stats "Docker容器管理"
+	echo "Docker容器列表"
+	docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}"
+	echo ""
+	echo "容器操作"
+	echo "------------------------"
+	echo "1. 创建新的容器"
+	echo "------------------------"
+	echo "2. 启动指定容器             6. 启动所有容器"
+	echo "3. 停止指定容器             7. 停止所有容器"
+	echo "4. 删除指定容器             8. 删除所有容器"
+	echo "5. 重启指定容器             9. 重启所有容器"
+	echo "------------------------"
+	echo "11. 进入指定容器           12. 查看容器日志"
+	echo "13. 查看容器网络           14. 查看容器占用"
+	echo "------------------------"
+	echo "0. 返回上一级选单"
+	echo "------------------------"
+	read -e -p "请输入你的选择: " sub_choice
+	case $sub_choice in
+		1)
+			send_stats "新建容器"
+			read -e -p "请输入创建命令: " dockername
+			$dockername
+			;;
+		2)
+			send_stats "启动指定容器"
+			read -e -p "请输入容器名（多个容器名请用空格分隔）: " dockername
+			docker start $dockername
+			;;
+		3)
+			send_stats "停止指定容器"
+			read -e -p "请输入容器名（多个容器名请用空格分隔）: " dockername
+			docker stop $dockername
+			;;
+		4)
+			send_stats "删除指定容器"
+			read -e -p "请输入容器名（多个容器名请用空格分隔）: " dockername
+			docker rm -f $dockername
+			;;
+		5)
+			send_stats "重启指定容器"
+			read -e -p "请输入容器名（多个容器名请用空格分隔）: " dockername
+			docker restart $dockername
+			;;
+		6)
+			send_stats "启动所有容器"
+			docker start $(docker ps -a -q)
+			;;
+		7)
+			send_stats "停止所有容器"
+			docker stop $(docker ps -q)
+			;;
+		8)
+			send_stats "删除所有容器"
+			read -e -p "$(echo -e "${gl_hong}注意: ${gl_bai}确定删除所有容器吗？(Y/N): ")" choice
+			case "$choice" in
+			  [Yy])
+				docker rm -f $(docker ps -a -q)
+				;;
+			  [Nn])
+				;;
+			  *)
+				echo "无效的选择，请输入 Y 或 N。"
+				;;
+			esac
+			;;
+		9)
+			send_stats "重启所有容器"
+			docker restart $(docker ps -q)
+			;;
+		11)
+			send_stats "进入容器"
+			read -e -p "请输入容器名: " dockername
+			docker exec -it $dockername /bin/sh
+			break_end
+			;;
+		12)
+			send_stats "查看容器日志"
+			read -e -p "请输入容器名: " dockername
+			docker logs $dockername
+			break_end
+			;;
+		13)
+			send_stats "查看容器网络"
+			echo ""
+			container_ids=$(docker ps -q)
+			echo "------------------------------------------------------------"
+			printf "%-25s %-25s %-25s\n" "容器名称" "网络名称" "IP地址"
+			for container_id in $container_ids; do
+				local container_info=$(docker inspect --format '{{ .Name }}{{ range $network, $config := .NetworkSettings.Networks }} {{ $network }} {{ $config.IPAddress }}{{ end }}' "$container_id")
+				local container_name=$(echo "$container_info" | awk '{print $1}')
+				local network_info=$(echo "$container_info" | cut -d' ' -f2-)
+				while IFS= read -r line; do
+					local network_name=$(echo "$line" | awk '{print $1}')
+					local ip_address=$(echo "$line" | awk '{print $2}')
+					printf "%-20s %-20s %-15s\n" "$container_name" "$network_name" "$ip_address"
+				done <<< "$network_info"
+			done
+			break_end
+			;;
+		14)
+			send_stats "查看容器占用"
+			docker stats --no-stream
+			break_end
+			;;
+		*)
+			break  # 跳出循环，退出菜单
+			;;
+	esac
+done
+}
+
+
+docker_image() {
+while true; do
+	clear
+	send_stats "Docker镜像管理"
+	echo "Docker镜像列表"
+	docker image ls
+	echo ""
+	echo "镜像操作"
+	echo "------------------------"
+	echo "1. 获取指定镜像             3. 删除指定镜像"
+	echo "2. 更新指定镜像             4. 删除所有镜像"
+	echo "------------------------"
+	echo "0. 返回上一级选单"
+	echo "------------------------"
+	read -e -p "请输入你的选择: " sub_choice
+	case $sub_choice in
+		1)
+			send_stats "拉取镜像"
+			read -e -p "请输入镜像名（多个镜像名请用空格分隔）: " imagenames
+			for name in $imagenames; do
+				echo -e "${gl_huang}正在获取镜像: $name${gl_bai}"
+				docker pull $name
+			done
+			;;
+		2)
+			send_stats "更新镜像"
+			read -e -p "请输入镜像名（多个镜像名请用空格分隔）: " imagenames
+			for name in $imagenames; do
+				echo -e "${gl_huang}正在更新镜像: $name${gl_bai}"
+				docker pull $name
+			done
+			;;
+		3)
+			send_stats "删除镜像"
+			read -e -p "请输入镜像名（多个镜像名请用空格分隔）: " imagenames
+			for name in $imagenames; do
+				docker rmi -f $name
+			done
+			;;
+		4)
+			send_stats "删除所有镜像"
+			read -e -p "$(echo -e "${gl_hong}注意: ${gl_bai}确定删除所有镜像吗？(Y/N): ")" choice
+			case "$choice" in
+			  [Yy])
+				docker rmi -f $(docker images -q)
+				;;
+			  [Nn])
+				;;
+			  *)
+				echo "无效的选择，请输入 Y 或 N。"
+				;;
+			esac
+			;;
+		*)
+			break  # 跳出循环，退出菜单
+			;;
+	esac
+done
+
+
+}
+
